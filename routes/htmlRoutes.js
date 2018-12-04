767ddc8b66011b5aef2dc0bc02957351d108b806
James Cherven <jcherven@mail.usf.edu>
2018-12-03 20:52:23 -0500

    Adds routing for create ppage

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,11 @@ module.exports = function(app) {
     });
   });
 
+  // Load Create page
+  app.get("/create", function(req, res){
+    res.render("create");
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
