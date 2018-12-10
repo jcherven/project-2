b6e92a8f0f6677a389ae87f5de0df43b72b29ecb
jygibson <jygibson922@gmail.com>
2018-12-09 19:16:46 -0500

    added search in public>js>index.js and it's throwing the results page and no errors, but not adding the info or the search term in the span field

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 // Get references to page elements
-$(document).ready(function() {
+$(document).ready(function () {
   //how to handle the submit on the create form, sends data to the database
   $(".btn").on("click", handleFormSubmit);
 
@@ -18,15 +18,32 @@ $(document).ready(function() {
       $.ajax("/api/recipes", {
         method: "POST",
         data: newRecipe
-      }).then(function() {
+      }).then(function () {
         location.reload();
       });
     }
   }
 
-  // function handleFormSubmit(event) {
-  //   event.preventDefault();
-  //   var searchTerm= $("#search").val().trim();
+  $(".search").on("click", handleSearchButton);
 
-  // }
+  function handleSearchButton(event) {
+    module.exports = function(app) {
+      event.preventDefault();
+      var searchTerm = $("#search").val().trim();
+      app.get("/api/recipes", function(req, res) {
+        db.Recipe.findAll({
+          where: {
+            keyword: searchTerm
+          }
+        }).then(function(body) {
+          res.render("results");
+          $("#search-term").text(searchTerm);
+          console.log(body);
+          for (i = 0; i < body.length; i++) {
+            $("#search-results").append($("<tr>").text(body[i]));
+          }
+        });
+      });
+    };
+  }
 });
